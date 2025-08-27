8227066e4f2a970901b9dd1952678a67e14f326e
FranckCV <167498675+FranckCV@users.noreply.github.com>
2025-08-27 11:37:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -892,5 +892,5 @@ def execute_post(name):
     return re
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+# if __name__ == "__main__":
+#     app.run(host='0.0.0.0', port=5000, debug=True)
