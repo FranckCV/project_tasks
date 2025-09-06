4fb090bc2d2cb91d471e4f378f1657bde960193c
FranckCV <francocor0105@gmail.com>
2025-09-06 12:34:42 -0500

    calendar v5

--- static/js/modal.js
+++ static/js/modal.js
@@ -4,10 +4,32 @@ document.querySelectorAll('.clickable-modal').forEach(btn => {
         let modalSpaceID = btn.dataset.target;
         const modalSpace = document.querySelector(`#${modalSpaceID}.modal_space`);
         modalSpace.classList.add('active');
-
+        
         const btnClose = modalSpace.querySelector('.close_modal ');
         btnClose.addEventListener('click', () => {
             modalSpace.classList.remove('active');
         });
+        
+
+        const modalNavs = modalSpace.querySelectorAll(`.modal_navs .nav`);
+        if (modalNavs) {
+            modalNavs.forEach(nav => {
+                nav.addEventListener('click',() => {
+                    let modalContID = nav.dataset.target;
+                    const containers = modalSpace.querySelectorAll(`.modal_navs_containers .modal_container `);
+                    containers.forEach(container => {
+                        if (container.id == modalContID) {
+                            container.style.display = 'flex';
+                        } else {
+                            container.style.display = 'none';
+                        }
+                    });
+                });
+            });
+        }
+
+
     });
 });
+
+
